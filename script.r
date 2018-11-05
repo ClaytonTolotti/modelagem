c1fcec18418e90f1a2b63fb59b275a01a344f572
Clayton Tolotti <claytontolotti@gmail.com>
2018-11-04 22:03:35 -0200

    Simulação do crescimento de um mosquito

---
+++ script.r
@@ -0,0 +1,115 @@
+#Fonte dos dados: http://www.inmet.gov.br/sonabra/pg_dspDadosCodigo_sim.php?QTgzOQ==
+setwd("~/git/modelagem")
+dados_temp <- read.csv(file="dados", header = TRUE, sep=",")
+dados <- data.frame(
+                    cod_estacao = c(dados_temp$codigo_estacao), 
+                    data = c(dados_temp$data),
+                    temp_min = c(dados_temp$temp_min),
+                    temp_max = c(dados_temp$temp_max)
+                  )
+#pegar as mínimas e máximas
+temperatura_base <- 8
+qnt_horas <- 24
+dias <- length(dados_temp$data)/qnt_horas
+minimas <- data.frame(
+                      d1 = min(dados$temp_min[1:qnt_horas]),
+                      d2 = min(dados$temp_min[(qnt_horas+1):(qnt_horas*2)]),
+                      d3 = min(dados$temp_min[(qnt_horas*2+1):(qnt_horas*3)]),
+                      d4 = min(dados$temp_min[(qnt_horas*3+1):(qnt_horas*4)]),
+                      d5 = min(dados$temp_min[(qnt_horas*4+1):(qnt_horas*5)]),
+                      d6 = min(dados$temp_min[(qnt_horas*5+1):(qnt_horas*6)]),
+                      d7 = min(dados$temp_min[(qnt_horas*6+1):(qnt_horas*7)]),
+                      d8 = min(dados$temp_min[(qnt_horas*7+1):(qnt_horas*8)]),
+                      d9 = min(dados$temp_min[(qnt_horas*8+1):(qnt_horas*9)]),
+                      d10 = min(dados$temp_min[(qnt_horas*9+1):(qnt_horas*10)]),
+                      d11 = min(dados$temp_min[(qnt_horas*10+1):(qnt_horas*11)]),
+                      d12 = min(dados$temp_min[(qnt_horas*11+1):(qnt_horas*12)]),
+                      d13 = min(dados$temp_min[(qnt_horas*12+1):(qnt_horas*13)]),
+                      d14 = min(dados$temp_min[(qnt_horas*13+1):(qnt_horas*14)]),
+                      d15 = min(dados$temp_min[(qnt_horas*14+1):(qnt_horas*15)]),
+                      d16 = min(dados$temp_min[(qnt_horas*15+1):(qnt_horas*16)]),
+                      d17 = min(dados$temp_min[(qnt_horas*16+1):(qnt_horas*17)]),
+                      d18 = min(dados$temp_min[(qnt_horas*17+1):(qnt_horas*18)]),
+                      d19 = min(dados$temp_min[(qnt_horas*18+1):(qnt_horas*19)]),
+                      d20 = min(dados$temp_min[(qnt_horas*19+1):(qnt_horas*20)]),
+                      d21 = min(dados$temp_min[(qnt_horas*20+1):(qnt_horas*21)]),
+                      d22 = min(dados$temp_min[(qnt_horas*21+1):(qnt_horas*22)]),
+                      d23 = min(dados$temp_min[(qnt_horas*22+1):(qnt_horas*23)]),
+                      d24 = min(dados$temp_min[(qnt_horas*23+1):(qnt_horas*24)]),
+                      d25 = min(dados$temp_min[(qnt_horas*24+1):(qnt_horas*25)]),
+                      d26 = min(dados$temp_min[(qnt_horas*25+1):(qnt_horas*26)]),
+                      d27 = min(dados$temp_min[(qnt_horas*26+1):(qnt_horas*27)]),
+                      d28 = min(dados$temp_min[(qnt_horas*27+1):(qnt_horas*28)]),
+                      d29 = min(dados$temp_min[(qnt_horas*28+1):(qnt_horas*29)]),
+                      d30 = min(dados$temp_min[(qnt_horas*29+1):(qnt_horas*30)]),
+                      d31 = min(dados$temp_min[(qnt_horas*30+1):(qnt_horas*31)])
+                    )
+
+maximas <- data.frame(
+                      d1 = max(dados$temp_max[1:qnt_horas]),
+                      d2 = max(dados$temp_max[(qnt_horas+1):(qnt_horas*2)]),
+                      d3 = max(dados$temp_max[(qnt_horas*2+1):(qnt_horas*3)]),
+                      d4 = max(dados$temp_max[(qnt_horas*3+1):(qnt_horas*4)]),
+                      d5 = max(dados$temp_max[(qnt_horas*4+1):(qnt_horas*5)]),
+                      d6 = max(dados$temp_max[(qnt_horas*5+1):(qnt_horas*6)]),
+                      d7 = max(dados$temp_max[(qnt_horas*6+1):(qnt_horas*7)]),
+                      d8 = max(dados$temp_max[(qnt_horas*7+1):(qnt_horas*8)]),
+                      d9 = max(dados$temp_max[(qnt_horas*8+1):(qnt_horas*9)]),
+                      d10 = max(dados$temp_max[(qnt_horas*9+1):(qnt_horas*10)]),
+                      d11 = max(dados$temp_max[(qnt_horas*10+1):(qnt_horas*11)]),
+                      d12 = max(dados$temp_max[(qnt_horas*11+1):(qnt_horas*12)]),
+                      d13 = max(dados$temp_max[(qnt_horas*12+1):(qnt_horas*13)]),
+                      d14 = max(dados$temp_max[(qnt_horas*13+1):(qnt_horas*14)]),
+                      d15 = max(dados$temp_max[(qnt_horas*14+1):(qnt_horas*15)]),
+                      d16 = max(dados$temp_max[(qnt_horas*15+1):(qnt_horas*16)]),
+                      d17 = max(dados$temp_max[(qnt_horas*16+1):(qnt_horas*17)]),
+                      d18 = max(dados$temp_max[(qnt_horas*17+1):(qnt_horas*18)]),
+                      d19 = max(dados$temp_max[(qnt_horas*18+1):(qnt_horas*19)]),
+                      d20 = max(dados$temp_max[(qnt_horas*19+1):(qnt_horas*20)]),
+                      d21 = max(dados$temp_max[(qnt_horas*20+1):(qnt_horas*21)]),
+                      d22 = max(dados$temp_max[(qnt_horas*21+1):(qnt_horas*22)]),
+                      d23 = max(dados$temp_max[(qnt_horas*22+1):(qnt_horas*23)]),
+                      d24 = max(dados$temp_max[(qnt_horas*23+1):(qnt_horas*24)]),
+                      d25 = max(dados$temp_max[(qnt_horas*24+1):(qnt_horas*25)]),
+                      d26 = max(dados$temp_max[(qnt_horas*25+1):(qnt_horas*26)]),
+                      d27 = max(dados$temp_max[(qnt_horas*26+1):(qnt_horas*27)]),
+                      d28 = max(dados$temp_max[(qnt_horas*27+1):(qnt_horas*28)]),
+                      d29 = max(dados$temp_max[(qnt_horas*28+1):(qnt_horas*29)]),
+                      d30 = max(dados$temp_max[(qnt_horas*29+1):(qnt_horas*30)]),
+                      d31 = max(dados$temp_max[(qnt_horas*30+1):(qnt_horas*31)])
+                    )
+
+medias <- data.frame( 
+                      m1 = (maximas$d1 - minimas$d1) - temperatura_base / 2,
+                      m2 = (maximas$d2 - minimas$d2) - temperatura_base / 2,
+                      m3 = (maximas$d3 - minimas$d3) - temperatura_base / 2,
+                      m4 = (maximas$d4 - minimas$d4) - temperatura_base / 2,
+                      m5 = (maximas$d5 - minimas$d5) - temperatura_base / 2,
+                      m6 = (maximas$d6 - minimas$d6) - temperatura_base / 2,
+                      m7 = (maximas$d7 - minimas$d7) - temperatura_base / 2,
+                      m8 = (maximas$d8 - minimas$d8) - temperatura_base / 2,
+                      m9 = (maximas$d9 - minimas$d9) - temperatura_base / 2,
+                      m10 = (maximas$d10 - minimas$d10) - temperatura_base / 2,
+                      m11 = (maximas$d11 - minimas$d11) - temperatura_base / 2,
+                      m12 = (maximas$d12 - minimas$d12) - temperatura_base / 2,
+                      m13 = (maximas$d13 - minimas$d13) - temperatura_base / 2,
+                      m14 = (maximas$d14 - minimas$d14) - temperatura_base / 2,
+                      m15 = (maximas$d15 - minimas$d15) - temperatura_base / 2,
+                      m16 = (maximas$d16 - minimas$d16) - temperatura_base / 2,
+                      m17 = (maximas$d17 - minimas$d17) - temperatura_base / 2,
+                      m18 = (maximas$d18 - minimas$d18) - temperatura_base / 2,
+                      m19 = (maximas$d19 - minimas$d19) - temperatura_base / 2,
+                      m20 = (maximas$d20 - minimas$d20) - temperatura_base / 2,
+                      m21 = (maximas$d21 - minimas$d21) - temperatura_base / 2,
+                      m22 = (maximas$d22 - minimas$d22) - temperatura_base / 2,
+                      m23 = (maximas$d23 - minimas$d23) - temperatura_base / 2,
+                      m24 = (maximas$d24 - minimas$d24) - temperatura_base / 2,
+                      m25 = (maximas$d24 - minimas$d25) - temperatura_base / 2,
+                      m26 = (maximas$d24 - minimas$d26) - temperatura_base / 2,
+                      m27 = (maximas$d24 - minimas$d27) - temperatura_base / 2,
+                      m28 = (maximas$d24 - minimas$d28) - temperatura_base / 2,
+                      m29 = (maximas$d24 - minimas$d29) - temperatura_base / 2,
+                      m30 = (maximas$d24 - minimas$d30) - temperatura_base / 2,
+                      m31 = (maximas$d24 - minimas$d31) - temperatura_base / 2
+                    )
+
